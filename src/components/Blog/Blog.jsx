0136f27d1eb354990c4659fba42919be639ac14b
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-10 14:58:27 +0600

    up

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 export const Blog = ({blog}) => {
-    const {author, cover, author_img, posted_date} = blog;
+    const {author, cover, author_img, posted_date,hashtags} = blog;
     console.log(blog);
   return (
     <div>
-        <img src={cover} alt={`Cover picture of the title ${author}`} />
+        <img className='h-80' src={cover} alt={`Cover picture of the title ${author}`} />
 
         <div className='flex justify-between'>
             <div  className='flex items-center gap-4'>
-                <img className='w-14 ' src={author_img} alt="" />
+                <img className='w-14 h-14 rounded-full ' src={author_img} alt="" />
                 <div>
                     <h3 className='text-2xl'>{author}</h3>
                     <p>{posted_date}</p>
@@ -20,6 +20,9 @@ export const Blog = ({blog}) => {
             </div>
         </div>
         <h2 className='text-4xl'>{author}</h2>
+        {
+            hashtags.map((hash) => <span><a href="">{hash}</a></span>)
+        }
     </div>
   )
 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -13,7 +13,7 @@ export const Blogs = () => {
     )
   return (
     <div className='w-2/3'>
-        <h1 className="text-4xl">Blogs {blogs.length}</h1>
+        <h1 className="text-4xl">Blogs: {blogs.length}</h1>
         {
             blogs.map(blog=> <Blog key={blog.id} blog={blog}/>)
         }
